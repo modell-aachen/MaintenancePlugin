f0072d3567e2a47177986a32c52fbfd2f4d3c83d
Maik Glatki <glatki@modell-aachen.de>
2015-05-11 15:35:10 +0200

    Add new check for Custom web skins and file permissions.
    * Check for wrong file permissions
    * Check for uncommon skin definitions in custom web.
    
    Ticket: None
    Release-Note(admin, feature){
    Add checks for file permissions and Custom web skin definitions.
    }

--- lib/Foswiki/Plugins/MaintenancePlugin.pm
+++ lib/Foswiki/Plugins/MaintenancePlugin.pm
@@ -111,6 +111,52 @@ my $checks = {
             }
             return $result;
         }
+    },
+    "general: customskins" => {
+        name => "Custom web skins",
+        description => "Custom web has only CustomSkins",
+        check => sub {
+            my $result = { result => 0 };
+            my @topics = grep(/(?<!^Custom)Skin/,Foswiki::Func::getTopicList( "Custom" ));
+            if ( scalar @topics ) {
+                $result->{result} = 1;
+                $result->{priority} = ERROR;
+                $result->{solution} = "Rename/restructure Skins in Custom web. Offending topics: " . join(", ", @topics);
+            }
+            return $result;
+        }
+    },
+    "general: filepermissions" => {
+        name => "File permissions",
+        description => "Some files has wrong permissions",
+        check => sub {
+            my $result = { result => 0 };
+            # Core module
+            use File::Find;
+            my @dirs = ( $Foswiki::cfg{DataDir}, $Foswiki::cfg{PubDir} );
+            our $direg = qr(^($Foswiki::cfg{DataDir})|($Foswiki::cfg{PubDir}));
+            our @offenders = ();
+            our @gcos = getpwuid( $< );
+            finddepth( { wanted => \&permissions, untaint => 1, untaint_pattern => /$direg/ }, @dirs );
+            # This implements:  find . ! -user www-data -or ! -perm -u+r -or \( -perm -u+w -name "*,v" \)
+            sub permissions{
+                my ( $dev, $ino, $mode, $nlink, $uid, $gid ) = lstat( $_ );
+                if ( ( ( $uid != $gcos[2] ) && ( ( $mode & 0400 ) == 0400 ) )
+                    or ( ( $File::Find::name =~ /,v$/) && ( ( $mode & 0600 ) == 0600 ) )
+                    or ( ( $File::Find::name =~ /\.txt$/) && ( ( $mode & 0600 ) != 0600 ) )
+                ) {
+                    push ( @offenders, $File::Find::name );
+                }
+            }
+            if ( scalar @offenders ) {
+                $result->{result} = 1;
+                $result->{priority} = ERROR;
+                $result->{solution} = "There exist " . scalar @offenders . " files and directories with wrong permissions.<br>" . join("<br>", @offenders);
+                my $help = '<br>Try one of these commands:<br><pre>    chown -R www-data:www-data .<br>    find -type d -exec chmod 755 {} \;<br>    chmod -R u+w *<br>    find . -type f -name "*,v" -exec chmod 444 {} \;</pre>';
+                $result->{solution} .= $help;
+            }
+            return $result;
+        }
     }
 };
 
