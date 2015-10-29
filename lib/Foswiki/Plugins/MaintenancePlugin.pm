f60cd1d918f2ed2de4c03b675683b2e4ea31a709
glatki <glatki@modell-aachen.de>
2015-10-29 10:39:58 +0100

    Set expected Foswiki version to 2.0.2.
    Ticket: None
    Release-Note: None

--- lib/Foswiki/Plugins/MaintenancePlugin.pm
+++ lib/Foswiki/Plugins/MaintenancePlugin.pm
@@ -330,7 +330,7 @@ sub maintenanceHandler {
         description => "Installed Foswiki release is not newest supported stable version.",
         check => sub {
             my $result = { result => 0 };
-            my $last = 'Foswiki-2.0.1';
+            my $last = 'Foswiki-2.0.2';
             if ( $Foswiki::RELEASE ne $last ) {
                 $result->{result} = 1;
                 $result->{priority} = $WARN;
