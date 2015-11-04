558b0af528a671e936f8bc036be59ebcef867005
glatki <glatki@modell-aachen.de>
2015-11-04 15:21:59 +0100

    Update file permission check for PlainFileStoreContrib.
    Ticket: None
    Release-Note(change,admin) {
    Update file permission check for Foswiki 2.0.x.
    }

--- lib/Foswiki/Plugins/MaintenancePlugin.pm
+++ lib/Foswiki/Plugins/MaintenancePlugin.pm
@@ -302,23 +302,23 @@ sub maintenanceHandler {
             my @dirs = ( $Foswiki::cfg{DataDir}, $Foswiki::cfg{PubDir} );
             our $direg = qr(^($Foswiki::cfg{DataDir})|($Foswiki::cfg{PubDir}));
             our @offenders = ();
-            our @gcos = getpwuid( $< );
-            File::Find::finddepth( { wanted => \&permissions, untaint => 1, untaint_pattern => /$direg/ }, @dirs );
-            # This implements:  find . ! -user www-data -or ! -perm -u+r -or \( -perm -u+w -name "*,v" \)
+            our @gcos = getpwuid($<);
+            File::Find::finddepth({ wanted => \&permissions, untaint => 1, untaint_pattern => /$direg/ }, @dirs);
+            # This implements:  find . ! -user www-data -or ! -perm -u+r
             sub permissions {
-                my ( $dev, $ino, $mode, $nlink, $uid, $gid ) = lstat( $_ );
-                if ( ( ( $uid != $gcos[2] ) && ( ( $mode & 0400 ) == 0400 ) )
-                    or ( ( $File::Find::name =~ /,v$/) && ( ( $mode & 0600 ) == 0600 ) )
-                    or ( ( $File::Find::name =~ /\.txt$/) && ( ( $mode & 0600 ) != 0600 ) )
+                my ($dev, $ino, $mode, $nlink, $uid, $gid) = lstat($_);
+                if (($uid != $gcos[2])          # Wrong uid
+                    or (($mode & 0400) != 0400) # Not readable
+                    or (($File::Find::name =~ /\.txt$/) && (($mode & 0600) != 0600 )) # .txt file not writable.
                 ) {
-                    push ( @offenders, $File::Find::name );
+                    push(@offenders, $File::Find::name);
                 }
             }
-            if ( scalar @offenders ) {
+            if (scalar @offenders) {
                 $result->{result} = 1;
                 $result->{priority} = $ERROR;
-                $result->{solution} = "There exist " . scalar @offenders . " files and directories with wrong permissions.<br>" . join("<br>", @offenders);
-                my $help = '<br>Try one of these commands in the foswiki directory:<br><pre>    chown -R www-data:www-data .<br>    find -type d -exec chmod 755 {} \;<br>    chmod -R u+w *<br>    find . -type f -name "*,v" -exec chmod 444 {} \;</pre>';
+                $result->{solution} = "There exist " . scalar @offenders . " files and directories with wrong permissions.<ul><li>" . join("</li><li>", @offenders) . '</li></ul>';
+                my $help = 'Try one of these commands in the foswiki directory:<br><pre>    chown -R www-data:www-data .<br>    find -type d -exec chmod 755 {} \;<br>    chmod -R u+w *</pre>';
                 $result->{solution} .= $help;
             }
             return $result;
